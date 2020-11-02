7ea46658e9581ece4d9451df6fdb812488e18145
Patryk Fryda <patryk.fryda@kolomololabs.com>
2020-11-02 13:39:42 +0100

    Refactor project structure

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { ThemeProvider } from "@shopify/restyle";
 import { NavigationContainer } from "@react-navigation/native";
-import theme from "./common/theme";
-import { HomeTabs } from "./navigation/HomeTabs";
-import { AuthStack } from "./navigation/AuthStack";
+import theme from "./src/common/theme";
+import { HomeTabs } from "./src/navigation/HomeTabs";
+import { AuthStack } from "./src/navigation/AuthStack";
 
 export default function App() {
   const [loggedIn, setLoggedIn] = React.useState(false);
