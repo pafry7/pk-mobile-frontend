cae3378eb51cd61a36876e34d45325af154de24f
Patryk Fryda <patryk.fryda@kolomololabs.com>
2021-01-03 15:16:01 +0100

    Create simple Map screen

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -2,60 +2,37 @@ import React from "react";
 import Box from "../components/Box";
 import Text from "../components/Text";
 import * as SecureStore from "expo-secure-store";
-import { Button } from "../components/Button";
 import { useAuth } from "../context/auth-context";
-import gql from "graphql-tag";
-import { useQuery } from "@apollo/react-hooks";
+import Gradient from "react-native-css-gradient";
+import MapView from "react-native-maps";
+import { StyleSheet, Dimensions } from "react-native";
+
+const styles = StyleSheet.create({
+  mapStyle: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 interface HomeProps {}
-const FETCH_USERS = gql`
-  query MyQuery {
-    test_users {
-      full_name
-    }
-  }
-`;
 
 const Map: React.FC<HomeProps> = ({}) => {
-  const { logout, user } = useAuth();
-  const { data, error, loading } = useQuery(FETCH_USERS);
   console.log("iam in map");
-  // const { data, error, loading } = { error: "x", data: "x", loading: false };
-
-  const printJwt = async () => {
-    const t = await SecureStore.getItemAsync("jwtToken");
-    console.log(t);
-  };
-
-  if (error) {
-    return (
-      <Box flex={1} alignItems="center" justifyContent="center">
-        <Text mb="m" color="primaryText">
-          Map
-        </Text>
-      </Box>
-    );
-  }
-
+  const { height, width } = Dimensions.get("window");
+  const LATITUDE = 50.07190457956277;
+  const LONGITUDE = 19.941856485955427;
+  const LATITUDE_DELTA = 0.005;
+  const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height); //minus bottom nav
   return (
     <Box flex={1} alignItems="center" justifyContent="center">
-      {loading ? (
-        <Text>loading... in map</Text>
-      ) : (
-        <>
-          <Text mb="m" color="primaryText">
-            Map
-          </Text>
-          <Text mb="xl" color="primaryText">
-            Hello {user.full_name}
-          </Text>
-          <Text mb="xl" color="primaryText">
-            Hello {data.test_users[0].full_name}
-          </Text>
-          <Button mb="xl" label="logout" onPress={logout}></Button>
-          <Button label="print jwt" onPress={printJwt}></Button>
-        </>
-      )}
+      <MapView
+        style={styles.mapStyle}
+        initialRegion={{
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+      ></MapView>
     </Box>
   );
 };
